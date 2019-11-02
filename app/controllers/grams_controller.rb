3b22ecbab3decd1fec6bffccedc5a870a9872a2e
Iuliia <julika27@gmail.com>
2019-11-02 19:13:40 +0000

    Added tests for gram show page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,6 +2,7 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @grams = Gram.all
   end
 
   def new
@@ -14,6 +15,13 @@ class GramsController < ApplicationController
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
     end
   end
 
