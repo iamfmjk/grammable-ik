420961c9d00f2861df60f7b9292b14b3b9b9b348
Iuliia <julika27@gmail.com>
2019-11-02 23:30:15 +0000

    added functionality and tests to protect grams from being edited and deleted by the wrong user

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @grams = Gram.all
@@ -26,11 +26,13 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.update_attributes(gram_params)
     if @gram.valid?
       redirect_to root_path
@@ -42,6 +44,7 @@ class GramsController < ApplicationController
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
@@ -53,8 +56,8 @@ def gram_params
   params.require(:gram).permit(:message)
 end
 
-def render_not_found
-  render plain: 'Not Found :(', status: :not_found
+def render_not_found(status=:not_found)
+  render plain: "#{status.to_s.titleize} :(", status: status
 end
 
 end
