241b2855f8e7f486a5cddd47af1214847135739b
Iuliia <julika27@gmail.com>
2019-11-02 22:40:05 +0000

    Added tests for saving the updates to grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -20,22 +20,34 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @gram.blank?
   end
 
   def edit
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not found', status: :not_found
+    return render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+    return render :edit, status: :unprocessable_entity
     end
   end
+
 
 private
 
 def gram_params
   params.require(:gram).permit(:message)
+end
+
+def render_not_found
+  render plain: 'Not Found :(', status: :not_found
 end
 
 end
