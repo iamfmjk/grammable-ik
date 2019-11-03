ff018cf8225ad7247d37a86f2bb234ffdc989042
Iuliia <julika27@gmail.com>
2019-11-03 21:49:51 +0000

    Added styles to navigation, new and index pages

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,7 +2,7 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @grams = Gram.all
+    @grams = Gram.order(created_at: :desc)
   end
 
   def new
